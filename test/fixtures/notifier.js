5ba0022c432268277b74da26d73dd73c5541526e
James Garbutt <43081j@users.noreply.github.com>
2026-09-22 12:26:45 +0100

    feat: rework failure messages and add tests (#3)

---
+++ test/fixtures/notifier.js
@@ -0,0 +1,22 @@
+import process from 'node:process';
+
+const { options, notify, tty } = JSON.parse(process.argv[2] ?? '{}');
+
+if (tty) {
+  process.stdout.isTTY = true;
+}
+
+const { notifier } = await import('../../lib/main.js');
+
+const instance = notifier({ name: 'fake-pkg', version: '1.0.0', ...options });
+
+instance.notify(notify);
+
+process.stdout.write(
+  JSON.stringify({
+    current: instance.current,
+    latest: instance.latest,
+    outdated: instance.outdated,
+    updateType: instance.updateType,
+  }),
+);
